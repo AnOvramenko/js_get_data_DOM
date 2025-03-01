9f3cc593721bdd2044c926f69e3fae3080276a44
Ovramenko Andrii <anovramenko2@gmail.com>
2025-03-01 15:42:33 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const allPopulation = [];
+
+const countries = document.querySelectorAll('.population');
+
+for (const country of countries) {
+  allPopulation.push(+country.innerHTML.split(',').join(''));
+}
+
+const total = document.querySelector('.total-population');
+const avarage = document.querySelector('.average-population');
+
+total.innerHTML = allPopulation.reduce((acc, num) => acc + num, 0);
+avarage.innerHTML = Math.round(+total.innerHTML / allPopulation.length);
